f6a90b48da61ee733b9865e1099148eb87d562ea
hangrybear666 <corey.lauster@gmail.com>
2024-08-19 18:37:32 +0200

    Cleaned up Jenkinsfile, moved  versioning to groovy script

--- java-app/script.groovy
+++ java-app/script.groovy
@@ -7,4 +7,18 @@ def extractRemoteProperties() {
   env.DOCKER_HUB_REPO_URL = props["DOCKER_HUB_REPO"]
 }
 
+def commitVersionUpdate() {
+    sh 'git config --global user.email "jenkins@example.com"'
+    sh 'git config --global user.name "jenkins"'
+
+    sh 'git status'
+    sh 'git branch'
+    sh 'git config --list'
+
+    sh "git remote set-url origin https://${USER}:${PASS}@github.com/hangrybear666/09-devops-bootcamp__aws.git"
+    sh 'git add .'
+    sh "git commit -m 'Updated Version to ${VERSION}'"
+    sh 'git push origin HEAD:main'
+}
+
 return this
